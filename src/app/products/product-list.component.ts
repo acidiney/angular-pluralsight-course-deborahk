4514c7575ad24f0bdbbd98aa4e00117ebbd3fb35
acidiney <acidineydias@gmail.com>
2020-07-07 15:57:17 +0100

    2 files modified

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,4 +4,34 @@ import { Component } from '@angular/core'
   selector: 'pm-products',
   templateUrl: './product-list.component.html'
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  pageTitle: string = 'ProductListComponent';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  products: any[] = [
+  {
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2019",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "https://images.unsplash.com/photo-1593642634627-6fdaf35209f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  },
+  {
+    "productId": 2,
+    "productName": "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2019",
+    "description": "15 gallon capacity rolling garden cart",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "https://images.unsplash.com/photo-1593642634627-6fdaf35209f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  }];
+
+  toogleImage () :void {
+    this.showImage = !this.showImage
+  }
+}
